9dce5af5edb90c435b6bf1cbbe76133572425bd6
Yi Chen <easoncy435@gmail.com>
2016-06-21 16:13:37 -0400

    Finished: async behavior best practices flow control objects and context

--- app/async.js
+++ app/async.js
@@ -2,10 +2,40 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+  	var deferred;
+
+  	deferred = Promise.defer();
+
+  	setTimeout(function(){
+  		if(true){
+  			deferred.resolve(value);
+  		} else{
+  			deferred.reject();
+  		}
+  	},100);
+
+  	return deferred.promise;
 
   },
 
   manipulateRemoteData: function(url) {
+  	var deferred, httpRequest;
 
+  	deferred = Promise.defer();
+    httpRequest = new XMLHttpRequest();
+
+    httpRequest.open('GET', url);
+    httpRequest.onload = function(){
+    	if(httpRequest.status === 200){
+    		data = JSON.parse(httpRequest.responseText).people.map(function(k){ return k.name }).sort();
+    		deferred.resolve(data);
+    	} else{
+    		deferred.reject();
+    	}
+    }
+
+    httpRequest.send();
+
+    return deferred.promise;
   }
 };
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+  	return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+  	constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+  	var iterateArray, key;
 
+  	iterateArray = [];
+
+  	for (key in obj){
+  		if(obj.hasOwnProperty(key)){
+  			iterateArray.push(key + ': ' + obj[key]);
+  		}
+  	}
+  	
+	return iterateArray;
   }
 };
